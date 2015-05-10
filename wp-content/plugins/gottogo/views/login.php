a7dd2c5b8b845b0844e107b37b109aa52e403852
kocko <konstantin.yovkov@gmail.com>
2015-05-10 15:25:31 +0300

    DEV-3 Implemented the functionality for forgotten password. Fixed a minor encryption defect for the login.

--- wp-content/plugins/gottogo/views/login.php
+++ wp-content/plugins/gottogo/views/login.php
@@ -138,8 +138,10 @@ function login_action() {
         $email = sanitize_text_field($_POST['login_email']);
         $password = sanitize_text_field($_POST['login_password']);
 
+        $passwordSalted = md5($password);
+
         $query = sprintf("SELECT * FROM users WHERE email='%s' AND password='%s'",
-                         mysql_real_escape_string($email), mysql_real_escape_string($password));
+                         mysql_real_escape_string($email), mysql_real_escape_string($passwordSalted));
 
         $result = mysql_query($query);
 
@@ -178,6 +180,36 @@ function register_action() {
             echo "Success";
         }
     }
+}
+
+function forgotten_password_action() {
+    if (isset($_POST['forgotten_password_action'])) {
+        $email = sanitize_text_field($_POST['forgotten_password_email']);
+        $newPassword = generate_new_password();
+
+        $newPasswordSalted = md5($newPassword);
+
+        $updateQuery = sprintf("UPDATE users set password = '%s' WHERE email = '%s'",
+                               mysql_real_escape_string($newPasswordSalted), mysql_real_escape_string($email));
+
+        $result = mysql_query($updateQuery, $GLOBALS['connection']);
+        if (!$result) {
+            die ("Could not update your password: " .mysql_error());
+        } else {
+            wp_mail($email, "New password for Gottoto", "Your new password is: " . $newPassword , null, null);
+            echo "Success"; //TODO
+        }
+    }
+}
+
+function generate_new_password($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
 
 ?>
