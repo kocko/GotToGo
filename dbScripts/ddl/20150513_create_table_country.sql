41466f84e68807175d7e27753a3eb1fd837848c0
kocko <konstantin.yovkov@gmail.com>
2015-05-18 22:15:16 +0300

    DEV-7 Added `country` table DDL and DML.

--- dbScripts/ddl/20150513_create_table_country.sql
+++ dbScripts/ddl/20150513_create_table_country.sql
@@ -3,8 +3,6 @@ CREATE TABLE `country` (
   `code` char(3) NOT NULL DEFAULT '',
   `name` char(52) NOT NULL DEFAULT '',
   `continent` enum('Asia','Europe','North America','Africa','Oceania','Antarctica','South America') NOT NULL DEFAULT 'Asia',
-  `region` char(26) NOT NULL DEFAULT '',
   `capital` int(11) DEFAULT NULL,
-  `code2` char(2) NOT NULL DEFAULT '',
   PRIMARY KEY (`Code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
