0b3c36986ec1f6362be92c94bec5129cbaadce21
kocko <konstantin.yovkov@gmail.com>
2015-05-18 22:21:23 +0300

    DEV-7 Added a foreign key constraint from city to country.

--- dbScripts/ddl/20150513_create_table_city.sql
+++ dbScripts/ddl/20150513_create_table_city.sql
@@ -6,5 +6,6 @@ CREATE TABLE `city` (
   `district` char(20) NOT NULL DEFAULT '',
   `population` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`),
-  KEY `CountryCode` (`CountryCode`)
+  KEY `CountryCode` (`CountryCode`),
+  CONSTRAINT `city_fk_1` FOREIGN KEY (`CountryCode`) REFERENCES `Country` (`Code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8;
