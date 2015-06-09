67abedb67fbc8a07af3e2c09a0b60d5226f16cb0
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-06-10 00:18:17 +0300

    DEV-11 Added all the categories and all the luggage item entries in the sql scripts

--- dbScripts/ddl/20150518_create_table_luggage_item.sql
+++ dbScripts/ddl/20150518_create_table_luggage_item.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS `luggage_item`;
 CREATE TABLE `luggage_item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(35) NOT NULL DEFAULT '',
-  `category` enum('Документи','Устройства','Облекло','Козметични и други','Полезни и занимателни', 'Непредметен багаж') NOT NULL,
+  `category` enum('Документи','Устройства','Облекло','Аксесоари','Козметични и гримове','За баня','Полезни','Занимателни','Допълнителни') NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8;
--- dbScripts/dml/20150518_luggage_item_values.sql
+++ dbScripts/dml/20150518_luggage_item_values.sql
@@ -1,10 +1,119 @@
+-- Документи
 INSERT INTO `luggage_item` VALUES (1,'Паспорт /Лична карта/','Документи');
 INSERT INTO `luggage_item` VALUES (2,'Бордна карта','Документи');
 INSERT INTO `luggage_item` VALUES (3,'Виза','Документи');
-INSERT INTO `luggage_item` VALUES (4,'Кредитна/Дебитна/ карта', 'Документи');
-INSERT INTO `luggage_item` VALUES (5,'Здравна застраховка','Devices');
-INSERT INTO `luggage_item` VALUES (6,'Бордна карта','Clothes');
-INSERT INTO `luggage_item` VALUES (7,'Джобна /Пътна/ карта','Clothes');
-INSERT INTO `luggage_item` VALUES (8,'Тениски','Clothes');
-INSERT INTO `luggage_item` VALUES (9,'Вратовръзка','Clothes');
-INSERT INTO `luggage_item` VALUES (10,'Аналгин','Medicines');
+INSERT INTO `luggage_item` VALUES (4,'Кредитна /Дебитна/ карта', 'Документи');
+INSERT INTO `luggage_item` VALUES (5,'Здравна застраховка','Документи');
+INSERT INTO `luggage_item` VALUES (6,'Бордна карта','Документи');
+INSERT INTO `luggage_item` VALUES (7,'Джобна /Пътна/ карта','Документи');
+INSERT INTO `luggage_item` VALUES (8,'Разговорник /Речник/','Документи');
+INSERT INTO `luggage_item` VALUES (9,'Пари /Валута/','Документи');
+
+-- Устройства
+INSERT INTO `luggage_item` VALUES (10,'Телефон','Устройства');
+INSERT INTO `luggage_item` VALUES (11,'Фотоапарат','Устройства');
+INSERT INTO `luggage_item` VALUES (12,'Обективи за фотоапарата','Устройства');
+INSERT INTO `luggage_item` VALUES (13,'Камера','Устройства');
+INSERT INTO `luggage_item` VALUES (14,'Лаптоп','Устройства');
+INSERT INTO `luggage_item` VALUES (15,'Таблет','Устройства');
+INSERT INTO `luggage_item` VALUES (16,'Електронен четец','Устройства');
+INSERT INTO `luggage_item` VALUES (17,'Слушалки','Устройства');
+INSERT INTO `luggage_item` VALUES (18,'Зарядни / Карти с памет','Устройства');
+INSERT INTO `luggage_item` VALUES (19,'Апарат за кръвно','Устройства');
+INSERT INTO `luggage_item` VALUES (20,'Ютия','Устройства');
+INSERT INTO `luggage_item` VALUES (21,'Сешоар','Устройства');
+INSERT INTO `luggage_item` VALUES (22,'Маша /Преса/ за коса','Устройства');
+INSERT INTO `luggage_item` VALUES (23,'Епилатор','Устройства');
+INSERT INTO `luggage_item` VALUES (24,'Машинка за бръснене','Устройства');
+INSERT INTO `luggage_item` VALUES (25,'Сешоар','Устройства');
+
+-- Облекло
+INSERT INTO `luggage_item` VALUES (26,'Яке /връхна дреха/','Облекло');
+INSERT INTO `luggage_item` VALUES (27,'Блузи / Тениски / Потници','Облекло');
+INSERT INTO `luggage_item` VALUES (28,'Панталони / Панталонки','Облекло');
+INSERT INTO `luggage_item` VALUES (29,'Гащеризон / Сукман','Облекло');
+INSERT INTO `luggage_item` VALUES (30,'Рокля / Пола','Облекло');
+INSERT INTO `luggage_item` VALUES (31,'Сако / Елек','Облекло');
+INSERT INTO `luggage_item` VALUES (32,'Костюм','Облекло');
+INSERT INTO `luggage_item` VALUES (33,'Шапка','Аксесоари');
+INSERT INTO `luggage_item` VALUES (34,'Ръкавици','Аксесоари');
+INSERT INTO `luggage_item` VALUES (35,'Шал','Аксесоари');
+INSERT INTO `luggage_item` VALUES (36,'Вратовръзка','Аксесоари');
+INSERT INTO `luggage_item` VALUES (37,'Папийонка','Аксесоари');
+INSERT INTO `luggage_item` VALUES (38,'Пижама','Облекло');
+INSERT INTO `luggage_item` VALUES (39,'Халат','Облекло');
+INSERT INTO `luggage_item` VALUES (40,'Сутиен','Облекло');
+INSERT INTO `luggage_item` VALUES (41,'Долно бельо','Облекло');
+INSERT INTO `luggage_item` VALUES (42,'Чорапи','Облекло');
+INSERT INTO `luggage_item` VALUES (43,'Чорапогащник','Облекло');
+
+-- Аксесоари
+INSERT INTO `luggage_item` VALUES (44,'Куфар /Сак/','Аксесоари');
+INSERT INTO `luggage_item` VALUES (45,'Чанта','Аксесоари');
+INSERT INTO `luggage_item` VALUES (46,'Портфейл /Портмоне/','Аксесоари');
+INSERT INTO `luggage_item` VALUES (47,'Ластик за коса','Аксесоари');
+INSERT INTO `luggage_item` VALUES (48,'Шнола / Фиби / Щипки за коса','Аксесоари');
+INSERT INTO `luggage_item` VALUES (50,'Очила / Лещи за очи','Аксесоари');
+INSERT INTO `luggage_item` VALUES (51,'Чадър / Дъждобран','Аксесоари');
+INSERT INTO `luggage_item` VALUES (52,'Бижута','Аксесоари');
+
+-- Козметични и гримове
+INSERT INTO `luggage_item` VALUES (54,'Четка за коса / Гребен','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (55,'Пяна /Гел / Лак за коса','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (56,'Крем за лице /за тяло/','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (57,'Парфюм / Дезодорант','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (58,'Афтършейв / Одеколон','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (59,'Лак за нокти','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (60,'Лакочистител','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (61,'Пила за нокти','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (62,'Нокторезачка','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (63,'Пинсета','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (64,'Спирала / Сенки / Молив за очи','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (65,'Очна линия','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (66,'Пудра / Фон дьо тен / Руж','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (67,'Червило / Гланц / Балсам за устни','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (68,'Памук / Тампони за почистване','Козметични и гримове');
+INSERT INTO `luggage_item` VALUES (69,'Плажно масло /олио/','Козметични и гримове');
+
+-- За баня
+INSERT INTO `luggage_item` VALUES (70,'Кърпи / Хавлии','За баня');
+INSERT INTO `luggage_item` VALUES (71,'Гъба за къпане','За баня');
+INSERT INTO `luggage_item` VALUES (72,'Четка за зъби','За баня');
+INSERT INTO `luggage_item` VALUES (73,'Паста за зъби','За баня');
+INSERT INTO `luggage_item` VALUES (74,'Вода за уста','За баня');
+INSERT INTO `luggage_item` VALUES (75,'Клечки за уши','За баня');
+INSERT INTO `luggage_item` VALUES (76,'Клечки / Конец за зъби','За баня');
+INSERT INTO `luggage_item` VALUES (77,'Пяна /Гел/ за бръснене','За баня');
+INSERT INTO `luggage_item` VALUES (78,'Самобръсначка','За баня');
+INSERT INTO `luggage_item` VALUES (79,'Шампоан','За баня');
+INSERT INTO `luggage_item` VALUES (80,'Балсам за коса','За баня');
+INSERT INTO `luggage_item` VALUES (81,'Сапун','За баня');
+INSERT INTO `luggage_item` VALUES (82,'Душ гел','За баня');
+
+-- Полезни
+INSERT INTO `luggage_item` VALUES (83,'Носни / Мокри кърпички','Полезни');
+INSERT INTO `luggage_item` VALUES (84,'Дамски превръзки / Тампони','Полезни');
+INSERT INTO `luggage_item` VALUES (85,'Презервативи','Полезни');
+INSERT INTO `luggage_item` VALUES (86,'Лекарства','Полезни');
+INSERT INTO `luggage_item` VALUES (87,'Храна','Полезни');
+INSERT INTO `luggage_item` VALUES (88,'Дъвки / Бонбони','Полезни');
+INSERT INTO `luggage_item` VALUES (89,'Напитки / Вода','Полезни');
+INSERT INTO `luggage_item` VALUES (90,'Лист и химикал','Полезни');
+INSERT INTO `luggage_item` VALUES (91,'Ключове','Полезни');
+INSERT INTO `luggage_item` VALUES (92,'Ножица','Полезни');
+
+-- Занимателни
+INSERT INTO `luggage_item` VALUES (93,'Книга / Вестник / Списание','Занимателни');
+INSERT INTO `luggage_item` VALUES (94,'Кръстословици / Судоку','Занимателни');
+INSERT INTO `luggage_item` VALUES (95,'Настолни игри','Занимателни');
+INSERT INTO `luggage_item` VALUES (96,'Карти за игра','Занимателни');
+INSERT INTO `luggage_item` VALUES (97,'Надуваеми предмети','Занимателни');
+INSERT INTO `luggage_item` VALUES (98,'Топка / Фрийзби','Занимателни');
+INSERT INTO `luggage_item` VALUES (99,'Водна екипировка','Занимателни');
+
+-- Допълнителни
+INSERT INTO `luggage_item` VALUES (100,'Инсталиране на приложения','Допълнителни');
+INSERT INTO `luggage_item` VALUES (101,'Зареждане на батерии','Допълнителни');
+INSERT INTO `luggage_item` VALUES (102,'Освобождаване на памет','Допълнителни');
+INSERT INTO `luggage_item` VALUES (103,'Сваляне на музика / филми','Допълнителни');
+INSERT INTO `luggage_item` VALUES (104,'Включване на роуминг','Допълнителни');
