cd603d140b7729c394948acf466e20eb6967fc1c
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-06-15 22:57:24 +0300

    Fixed broken quotes

--- dbScripts/dml/20150615_budget_planning_values.sql
+++ dbScripts/dml/20150615_budget_planning_values.sql
@@ -1,31 +1,31 @@
 -- Преди пътуване
-INSERT INTO `budget_planning` VALUES (1, ''Пазар преди тръгване'', ''1'', ''Преди пътуване'');
-INSERT INTO `budget_planning` VALUES (2, ''Паспорт / Виза / Пътни документи'', ''1'', ''Преди пътуване'');
-INSERT INTO `budget_planning` VALUES (3, ''Здравна застраховка'', ''0'', ''Преди пътуване'');
-INSERT INTO `budget_planning` VALUES (4, ''Имунизации'', ''0'', ''Преди пътуване'');
+INSERT INTO `budget_planning` VALUES (1, 'Пазар преди тръгване', '1', 'Преди пътуване');
+INSERT INTO `budget_planning` VALUES (2, 'Паспорт / Виза / Пътни документи', '1', 'Преди пътуване');
+INSERT INTO `budget_planning` VALUES (3, 'Здравна застраховка', '0', 'Преди пътуване');
+INSERT INTO `budget_planning` VALUES (4, 'Имунизации', '0', 'Преди пътуване');
 
 -- Общи
-INSERT INTO `budget_planning` VALUES (5, ''Цена на нощувка'', ''0'', ''Общи'');
-INSERT INTO `budget_planning` VALUES (6, ''Джобни'', ''0'', ''Общи'');
-INSERT INTO `budget_planning` VALUES (7, ''Непредвидени разходи'', ''1'', ''Общи'');
-INSERT INTO `budget_planning` VALUES (8, ''Телефонни разходи'', ''1'', ''Общи'');
+INSERT INTO `budget_planning` VALUES (5, 'Цена на нощувка', '0', 'Общи');
+INSERT INTO `budget_planning` VALUES (6, 'Джобни', '0', 'Общи');
+INSERT INTO `budget_planning` VALUES (7, 'Непредвидени разходи', '1', 'Общи');
+INSERT INTO `budget_planning` VALUES (8, 'Телефонни разходи', '1', 'Общи');
 
 -- Транспорт
-INSERT INTO `budget_planning` VALUES (9, ''Транспортни разходи'', ''0'', ''Транспорт'');
-INSERT INTO `budget_planning` VALUES (10, ''Разходи за багаж'', ''0'', ''Транспорт'');
-INSERT INTO `budget_planning` VALUES (11, ''Билети за градски транспорт'', ''0'', ''Транспорт'');
-INSERT INTO `budget_planning` VALUES (12, ''Кола под наем'', ''1'', ''Транспорт'');
-INSERT INTO `budget_planning` VALUES (13, ''Платен паркинг'', ''1'', ''Транспорт'');
-INSERT INTO `budget_planning` VALUES (14, ''Гориво'', ''1'', ''Транспорт'');
+INSERT INTO `budget_planning` VALUES (9, 'Транспортни разходи', '0', 'Транспорт');
+INSERT INTO `budget_planning` VALUES (10, 'Разходи за багаж', '0', 'Транспорт');
+INSERT INTO `budget_planning` VALUES (11, 'Билети за градски транспорт', '0', 'Транспорт');
+INSERT INTO `budget_planning` VALUES (12, 'Кола под наем', '1', 'Транспорт');
+INSERT INTO `budget_planning` VALUES (13, 'Платен паркинг', '1', 'Транспорт');
+INSERT INTO `budget_planning` VALUES (14, 'Гориво', '1', 'Транспорт');
 
 -- Хранения
-INSERT INTO `budget_planning` VALUES (15, ''Закуски'', ''0'', ''Хранения'');
-INSERT INTO `budget_planning` VALUES (16, ''Храна на ден'', ''0'', ''Хранения'');
-INSERT INTO `budget_planning` VALUES (17, ''Заведения'', ''1'', ''Хранения'');
-INSERT INTO `budget_planning` VALUES (18, ''Нощен живот'', ''1'', ''Хранения'');
+INSERT INTO `budget_planning` VALUES (15, 'Закуски', '0', 'Хранения');
+INSERT INTO `budget_planning` VALUES (16, 'Храна на ден', '0', 'Хранения');
+INSERT INTO `budget_planning` VALUES (17, 'Заведения', '1', 'Хранения');
+INSERT INTO `budget_planning` VALUES (18, 'Нощен живот', '1', 'Хранения');
 
 -- Дейности / Забележителности
-INSERT INTO `budget_planning` VALUES (19, ''Пазар'', ''1'', ''Дейности / Забележителности'');
-INSERT INTO `budget_planning` VALUES (20, ''Подаръци / Сувенири'', ''1'', ''Дейности / Забележителности'');
-INSERT INTO `budget_planning` VALUES (21, ''Туристическа обиколка'', ''0'', ''Дейности / Забележителности'');
-INSERT INTO `budget_planning` VALUES (22, ''Вход за атракции'', ''0'', ''Дейности / Забележителности'');
+INSERT INTO `budget_planning` VALUES (19, 'Пазар', '1', 'Дейности / Забележителности');
+INSERT INTO `budget_planning` VALUES (20, 'Подаръци / Сувенири', '1', 'Дейности / Забележителности');
+INSERT INTO `budget_planning` VALUES (21, 'Туристическа обиколка', '0', 'Дейности / Забележителности');
+INSERT INTO `budget_planning` VALUES (22, 'Вход за атракции', '0', 'Дейности / Забележителности');
