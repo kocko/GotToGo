c6ac5deb020e9b698d4ca9ae8551f6cb9f79a58e
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-06-15 22:44:43 +0300

    DEV-19 added the DML script for `budget_planning`

--- dbScripts/ddl/20150615_create_table_budget_planning.sql
+++ dbScripts/ddl/20150615_create_table_budget_planning.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS `budget_planning`;
 CREATE TABLE `budget_planning` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(35) NOT NULL DEFAULT '',
-  `shared` enum ('0', '1') DEFAULT '0',
-  `category` enum('Документи','Устройства','Облекло','Аксесоари','Козметични и гримове','За баня','Полезни','Занимателни','Допълнителни') NOT NULL,
+  `shared` enum ('0', '1') DEFAULT '0'  ,
+  `category` enum('Преди пътуване', 'Общи', 'Транспорт', 'Хранения', 'Дейности / Забележителности') NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
