632739a53a6f38ce53740f46df560159a3f51806
Konstantin Yovkov <konstantin.yovkov@tis.biz>
2015-06-20 18:52:41 +0300

    Fixed an error with a button and reverted it back to be a hyperlink

--- wp-content/plugins/gottogo/views/login/forgotten_password.php
+++ wp-content/plugins/gottogo/views/login/forgotten_password.php
@@ -2,6 +2,13 @@
 
 function getForgottenPasswordForm() {
 ?>
+    <script type="text/css">
+        a.unclickable {
+            opacity: 0.5;
+            pointer-events: none;
+            cursor: default;
+        }
+    </script>
     <script>
         jQuery(function(){
             jQuery("#send_new_password").click(function(){
@@ -21,8 +28,11 @@ function getForgottenPasswordForm() {
         });
 
         function disableFormButtonForgotten(result, formButtonId) {
-            alert(result);
-            jQuery('#' + formButtonId).prop('disabled', result == 0);
+            if (result == 0) {
+
+
+                jQuery('#' + formButtonId).prop('disabled', result == 0);
+            }
         }
     </script>
 
@@ -35,14 +45,14 @@ function getForgottenPasswordForm() {
                             <label for="forgotten_password_email" class="control-label">Електронна поща</label>
                             <input type="email" class="form-control" id="forgotten_password_email" name="forgotten_password_email"
                                    value="" placeholder="example@gmail.com" autocomplete="off"
-                                   required onblur="validateEmail('forgotten_password_email', 'forgotten_password_email_taken_alert', 0, function (result, formButtonId) { jQuery('#' + formButtonId).prop('disabled', result == 0);} , 'send_new_password')">
+                                   required onblur="validateEmail('forgotten_password_email', 'forgotten_password_email_taken_alert', 0, function (result, formButtonId) {if (result == 0) { jQuery('#' + formButtonId).unbind('click');} else { jQuery('#' + formButtonId).bind('click');} } , 'send_new_password')">
                             <span class="help-block"></span>
                         </div>
                         <div class="alert alert-danger alert-dismissable collapse" role="alert" id="forgotten_password_email_taken_alert">
                             Няма регистриран потребител с тази електронна поща!
                         </div>
-                        <button class="btn btn-success btn-block"
-                           id="send_new_password" name="send_new_password">Изпрати нова парола</button>
+                        <a class="btn btn-success btn-block"
+                           id="send_new_password" name="send_new_password">Изпрати нова парола</a>
                     </form>
                 </div>
             </div>
